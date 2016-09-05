6d8d7bf815e0da6d2370dbb5410d1de7ff1f646e
adamsh25 <tso250@gmail.com>
2016-09-06 00:14:56 +0300

    https://github.com/adamsh25/Appy/issues/11

---
+++ AppyWebApp/utils/lib/dataSourceFactory/facebook/event.js
@@ -0,0 +1,13 @@
+/**
+ * Created by adam on 06/09/2016.
+ */
+var FacebookPageDataSource  = function FacebookPageDataSource(options) {
+    this.options = options;
+};
+
+FacebookPageDataSource.prototype.fetchData = function () {
+    console.log("Start Fetching Data From Facebook Page");
+    console.log(this.options);
+};
+
+module.exports = FacebookPageDataSource;
